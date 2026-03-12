3e21ccbe2ed34e274e7717e00974fcc95d113a6f
zhangxinmin <zhangxinmin@bilibili.com>
2026-03-12 17:59:09 +0800

    feat: macos点击程序坞中的图标可打开主界面

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -7,6 +7,14 @@ class AppDelegate: FlutterAppDelegate {
     return false  // keep alive when window is hidden to tray
   }
 
+  override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows: Bool) -> Bool {
+    if !hasVisibleWindows {
+      sender.windows.first?.makeKeyAndOrderFront(nil)
+      sender.activate(ignoringOtherApps: true)
+    }
+    return true
+  }
+
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
